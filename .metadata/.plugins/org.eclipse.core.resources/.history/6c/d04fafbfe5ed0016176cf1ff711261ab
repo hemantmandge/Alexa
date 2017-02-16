2fc9b3624714842716f3a7d03a3b69ff03792e5c
hemant.mandge <hemant.mandge@gmail.com>
2017-02-16 11:44:21 +0530

    Base Version

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/6c/d04fafbfe5ed0016176cf1ff711261ab
@@ -0,0 +1,6 @@
+package com.ge.code.generate.request.service;
+
+public interface CodeGenRequestService {
+	public String getAllCodeGenRequests();
+	public void addCodeGenRequest();
+}
---
+++ CodeGenUtility/src/main/java/com/ge/DataCodeGenerationUtilityApplication.java
@@ -0,0 +1,12 @@
+package com.ge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCodeGenerationUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCodeGenerationUtilityApplication.class, args);
+	}
+}
