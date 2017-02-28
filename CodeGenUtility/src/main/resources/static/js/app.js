9f82efa8ea58e8a73e20a9836e72888f53035989
hemant.mandge <hemant.mandge@gmail.com>
2017-02-28 17:13:52 +0530

    UI Changes for Loader and File

--- CodeGenUtility/src/main/resources/static/js/app.js
+++ CodeGenUtility/src/main/resources/static/js/app.js
@@ -1,10 +1,11 @@
-var app = angular.module("utilityApp", ['ngRoute', 'angularUtils.directives.dirPagination', 'ui.bootstrap', 'utility.config']);
+var app = angular.module("utilityApp", ['ngRoute', 'angularUtils.directives.dirPagination', 'ui.bootstrap', 'utility.config','ngPatternRestrict']);
 app.controller("utilityCtrl", ['$scope', '$rootScope', '$window', '$location', '$http', '$uibModal', 'Base_URL', function($scope, $rootScope, $window, $location, $http, $uibModal, Base_URL) {
  $scope.username = "admin";
  $scope.password = "1234";
  $scope.showHideHDFS = false;
  $scope.selected = false;
  $scope.rdbms = {};
+ $scope.file={};
  $scope.disabled = false;
  $scope.loading = false;
  $scope.welcome = false;
@@ -24,6 +25,41 @@ app.controller("utilityCtrl", ['$scope', '$rootScope', '$window', '$location', '
 $scope.showbutton=true;
 $scope.showbuttonDisc=false;
 $scope.connectDisble=false;
+$scope.showDBName=true;
+/**
+ * calling web services for File Data source Start 
+ */
+//alert("outsideggtnehayadav06 ");
+$scope.loadFileInitialData=function()
+{
+	 $scope.file.sourceType="FILE";
+	//$scope.sourceType="FILE";
+	//alert("in1 "+$scope.sourceType);
+//	alert("in 2 "+$scope.file.sourceType);
+	
+	//alert("loadFile");
+	
+	  $http.get("resources/findByType?type=FILETYPE").then(function(response) {
+		   //alert(response.data);
+		   $scope.fileType = response.data; //ajax request to fetch data into $scope.data
+		  // $scope.file.fileType= response.data[0];
+		   
+		  });
+	  
+	  $http.get("resources/findByName?name=SFTP").then(function(response) {
+		    $scope.SFTPserverIPList = response.data; //ajax request to fetch data into $scope.data
+		   });
+	  
+	  $scope.rdbms.targetType = "HIVE";
+	  if ($scope.rdbms.targetType == 'HIVE') {
+	   //alert("hellp trt ddd"+$scope.rdbms.targetType);
+	   $scope.rdbms.loadType = "FULL";
+	   $http.get("resources/findByType?type=LOADTYPE").then(function(response) {
+	    $scope.loadType = response.data; //ajax request to fetch data into $scope.data
+	   });
+	  }
+	};
+//calling web services for File Data source End
 
 
 $scope.enableForm =function(){
@@ -37,21 +73,15 @@ if($scope.showbuttonDisc == true){
     
 }
 
-$scope.go =function(){
- //alert ("in go button");
-  $rootScope.showHome=false;
-     //alert("hi in go button"+$rootScope.showHome);   
-        }
+  $scope.navigatePath =function(){
+    $rootScope.showHome=false;
+    }
 
 
 
- if ($location.path() == "/home") {
-  $scope.homeShow = false;
- } else {
-  $scope.homeShow = true;
- }
+ 
  $scope.onCancel = function() {
-  //  alert ("in cancel button");
+  //  //alert ("in cancel button");
   $scope.rdbms = {};
   $location.path('/home');
  }
@@ -60,12 +90,12 @@ $scope.go =function(){
    $scope.datadbsrc = response.data; //ajax request to fetch data into $scope.data
   });
   $http.get("resources/findByType?type=TARGETCONNECTION").then(function(response) {
-   //alert(response.data);
+   ////alert(response.data);
    $scope.targetcon = response.data; //ajax request to fetch data into $scope.data
   });
   $scope.rdbms.targetType = "HIVE";
   if ($scope.rdbms.targetType == 'HIVE') {
-   //alert("hellp trt ddd"+$scope.rdbms.targetType);
+   ////alert("hellp trt ddd"+$scope.rdbms.targetType);
    $scope.rdbms.loadType = "FULL";
    $http.get("resources/findByType?type=LOADTYPE").then(function(response) {
     $scope.loadType = response.data; //ajax request to fetch data into $scope.data
@@ -76,23 +106,25 @@ $scope.go =function(){
   $scope.loading = true;
   $scope.welcome = true;
  }, 1000);
- //alert(Base_URL);
+ ////alert(Base_URL);
  $scope.showHiveBlk = function(data) {
   if (data == 'HIVE') {
-   //  alert(data);
+   //  //alert(data);
    $scope.disabled = false;
    $scope.isdisabled = false;
+   $scope.hivetabledisabled=false;
    $http.get("resources/findByType?type=LOADTYPE").then(function(response) {
     $scope.loadType = response.data; //ajax request to fetch data into $scope.data
    });
   } else {
-   //alert(data);
+   ////alert(data);
    $scope.rdbms.targetTableName = "";
    $scope.rdbms.targetDBName = "";
    $scope.rdbms.hiveTableType = "";
    $scope.rdbms.targetPartitionKey = "";
    $scope.disabled = true;
    $scope.isdisabled = true;
+   $scope.hivetabledisabled= true;
    $http.get("resources/findByName?name=FULL").then(function(response) {
     $scope.loadType = response.data; //ajax request to fetch data into $scope.data
    });
@@ -109,7 +141,7 @@ $scope.go =function(){
  $scope.submitResult = function(datasrc) {
   $scope.homeShow = true;
   $rootScope.showHome=true;
-  // alert("submitResult" + $scope.neha);
+  // //alert("submitResult" + $scope.neha);
   $scope.datasrc = datasrc;
   if (datasrc == 'RDBMS') {
    $location.path('/RDBMS');
@@ -134,29 +166,42 @@ $scope.go =function(){
   $scope.datasrc = datasrc;
   $scope.datadbconn = []; //declare an empty array
   $http.get("resources/findByName?name=" + datasrc).then(function(response) {
-   // alert(response);
+   // //alert(response);
    $scope.datadbconn = response.data; //ajax request to fetch data into $scope.data
   });
+ if(datasrc=="ORACLE")
+	 {
+	 //alert("in if ");
+	 
+	 $scope.showDBName=true;
+	 }
+ else
+	 {
+	 //alert("in else");
+	 $scope.showDBName=false;
+	 }
  }
  $scope.getSID = function(srctype) {
-  // alert("hjjkhjk"+$scope.dbSID);
+  // //alert("hjjkhjk"+$scope.dbSID);
   $scope.srcdbport = srctype;
-  //alert($scope.rdbms.srcdbport);
+  ////alert($scope.rdbms.srcdbport);
   $scope.datadbSID = []; //declare an empty array
   $http.get("resources/findByName?name=" + srctype).then(function(response) {
-   //  alert(response);
+   //  //alert(response);
    $scope.datadbSID = response.data; //ajax request to fetch data into $scope.data
   });
  }
  $scope.connectDB = function(username, password, host, dbName, rdbmsName) {
      $scope.dataLoading = true;
-    
+    $scope.loadingBackgronud = true;
+    ////alert( $scope.loadingBackgronud);
   $scope.databaseSchema = [];
   $http.get("resources/getSchemaDetails?userid=" + username + "&password=" + password + "&host=" + host + "&database=" + dbName + "&rdbmsName=" + rdbmsName + "").then(function(response) {
-   // alert("http://10.174.132.12:8080/resources/getSchemaDetails?userid="+username+"&password="+password+"&host="+host+"&database="+database+"&rdbmsName="+rdbmsName+"");
+   // //alert("http://10.174.132.12:8080/resources/getSchemaDetails?userid="+username+"&password="+password+"&host="+host+"&database="+database+"&rdbmsName="+rdbmsName+"");
    $scope.databaseSchema = response.data; //ajax request to fetch data into $scope.data
    if (response.data) {
-       $scope.dataLoading = false;
+    $scope.dataLoading = false;
+    $scope.loadingBackgronud = false;
     $scope.readonlyAttr=true;
     $scope.connectDisble=true;
 
@@ -176,6 +221,7 @@ $scope.go =function(){
      templateUrl: 'html/errorpopupconnect.html',
     });
     $scope.dataLoading = false;
+    $scope.loadingBackgronud = false;
    }
   });
  }
@@ -183,20 +229,34 @@ $scope.go =function(){
   $scope.tables = []; //declare an empty array
   $http.get("resources/getTables?userid=" + username + "&password=" + password + "&host=" + host + "&database=" + database + "&rdbmsName=" + rdbmsName + "&schema=" + dbschema + "").then(function(response) {
    $scope.tables = response.data; //ajax request to fetch data into $scope.data
-   $scope.sourceTables = response.data.slice();
-   $scope.tables.splice(0, 0, "Select All");
+  
+   if( $scope.tables.length>1)
+	   {
+	 //  //alert("in if "+response.data);
+	   $scope.sourceTables = response.data.slice();
+	   $scope.tables.splice(0, 0, "Select All");
+	   }
+
   });
  }
  $scope.sourceCols={};
  $scope.getCols = function(username, password, host, database, rdbmsName, tableName, dbschema) {
-  // alert(tableName);
+  // //alert(tableName);
+
+  var tbllen =tableName.length;
   $scope.cols = []; //declare an empty array
   if (tableName.length <= 1) {
    $http.get("/resources/getColumns?userid=" + username + "&password=" + password + "&host=" + host + "&database=" + database + "&rdbmsName=" + rdbmsName + "&tableName=" + tableName + "&schema=" + dbschema + "").then(function(response) {
-    //   alert("http://10.174.132.12:8080/resources/getColumns?userid="+username+"&password="+password+"&host="+host+"&database="+database+"&rdbmsName="+rdbmsName+"&tableName="+tableName+"");
+    //   //alert("http://10.174.132.12:8080/resources/getColumns?userid="+username+"&password="+password+"&host="+host+"&database="+database+"&rdbmsName="+rdbmsName+"&tableName="+tableName+"");
     $scope.cols = response.data; //ajax request to fetch data into $scope.data
-    $scope.sourceCols = response.data.slice();
-    $scope.cols.splice(0, 0, "Select All");
+ //alert("table length --"+tbllen);
+    if(tbllen==1&&   $scope.cols.length>1){
+    	  //alert("1");
+    	   $scope.sourceCols = response.data.slice();
+      
+      $scope.cols.splice(0, 0, "Select All");
+    }
+   
    });
   }
   $scope.srcclumn = $scope.cols;
@@ -209,16 +269,21 @@ $scope.go =function(){
   });
  }
  $scope.changePartitinKey = function(tabletype) {
-  //alert(tabletype);
+  ////alert(tabletype);
   console.log('type is', tabletype);
   if (tabletype == 'NON-PARTITIONED') {
-   $scope.isdisabled = true;
+	  //alert("in if");
+    $scope.isdisabled = true;
+   $scope.hivetabledisabled=false;
   } else {
+	  //alert("in else");
    $scope.isdisabled = false;
+ 
   }
  }
  $scope.disableDrop = function(selectedItem) {
   if (selectedItem.length >= 2) {
+	  //alert("disableDrop");
    $scope.disabledMultiple = true;
    $scope.isdisabled = true;
    $scope.rdbms.hiveTableType = "NON-PARTITIONED";
@@ -234,7 +299,8 @@ $scope.go =function(){
   }
  }
  $scope.postdata = function(rdbmsdata) {
-  //alert($scope.rdbmsForm.$valid);
+  ////alert($scope.rdbmsForm.$valid);
+   $scope.submitted = true;
   if ($scope.rdbmsForm.$valid) {
    var data = rdbmsdata;
     $scope.dataLoading = true;
@@ -264,7 +330,7 @@ $scope.go =function(){
  };
  
  $scope.postdataFile = function(fileData) {
-	  //alert($scope.rdbmsForm.$valid);
+	  ////alert($scope.rdbmsForm.$valid);
 	  if ($scope.fileForm.$valid) {
 	   var data = fileData;
 	    $scope.dataLoading = true;
@@ -296,18 +362,19 @@ $scope.go =function(){
  $scope.hiveTableType = {};
  $scope.loadHiveTableType = function() {
   $http.get("resources/findByType?type=HIVETABLETYPE").then(function(response) {
-   //  alert(response);
+   //  //alert(response);
    $scope.hiveTableType = response.data; //ajax request to fetch data into $scope.data
   });
  }
  $scope.colRequiredchk = true;
  $scope.onSelcted2 = function(selectedItem) {
   if (selectedItem == 'Select All') {
-   // alert($scope.tables);
+    //alert("onSelcted2");
    $scope.rdbms.hiveTableType = "NON-PARTITIONED";
    $scope.rdbms.sourceTableNames = $scope.sourceTables;
    $scope.selectedAllTables = !$scope.selectedAllTables;
    $scope.disabledMultiple = true;
+   $scope.hivetabledisabled=true;
    $scope.colRequiredchk = false;
    $scope.isdisabled = true;
     $scope.rdbms.sourceColumnNames="";
