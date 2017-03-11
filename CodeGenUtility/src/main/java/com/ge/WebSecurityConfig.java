608083fb0c7b10ca8d135defad9b711e7679587b
hemant.mandge <hemant.mandge@gmail.com>
2017-03-11 17:28:03 +0530

    Code for login. js is pending

--- CodeGenUtility/src/main/java/com/ge/WebSecurityConfig.java
+++ CodeGenUtility/src/main/java/com/ge/WebSecurityConfig.java
@@ -1,31 +1,77 @@
 package com.ge;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        /*http
-            .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();*/
-    	http
-        .httpBasic()
-      .and()
-        .authorizeRequests()
-          .antMatchers("/#/index.html", "/home.html", "/#/login.html", "/").permitAll()
-          .anyRequest().authenticated();
+	@Autowired
+    private RestAuthenticationSuccessHandler restAuthenticationSuccessHandler;
+	
+	@Autowired
+	private CustomAuthenticationProvider customAuthenticationProvider;
+
+	@Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.inMemoryAuthentication()
+        .withUser("user").password("password").roles("USER");
+    }
+
+    @Autowired
+    private RestAuthenticationFailureHandler restAuthenticationFailureHandler;
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**", "/data/**", "/font/**","/img/**", "/js/**","/lib/**","/html/index.html", "/html/login.html");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			//.authenticationProvider(customAuthenticationProvider)
+			.csrf().disable()
+			.authorizeRequests()
+				.antMatchers("/index.html", "/html/home.html", "/html/login.html", "/").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginProcessingUrl("/authenticate")
+				.loginPage("/html/login.html").permitAll()
+				.successForwardUrl("/home")
+				.failureUrl("/")
+				.usernameParameter("username")
+				.passwordParameter("password")
+				.successHandler(restAuthenticationSuccessHandler)
+				.failureHandler(restAuthenticationFailureHandler)
+				.and()
+			.logout()
+				.logoutUrl("/logout")
+	            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
+	            .deleteCookies("JSESSIONID")
+	            .permitAll();
+		
+		 /* http .httpBasic() .and() .authorizeRequests()
+		  .antMatchers("/#/index.html", "/home.html", "/#/login.html",
+		  "/").permitAll() .anyRequest().authenticated();*/
+		 
+		/*
+		 * http .formLogin().and() .authorizeRequests()
+		 * .antMatchers("/index.html", "/home.html", "/login.html",
+		 * "/").permitAll() .anyRequest().authenticated();
+		 */
+	}
+	 private CsrfTokenRepository csrfTokenRepository() {
+    	HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+    	repository.setHeaderName("X-XSRF-TOKEN");
+    	return repository;
     }
 }
