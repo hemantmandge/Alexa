2f0c0030b39b9bc308aa3f0465be8ba85c2bf1f6
hemant.mandge <hemant.mandge@gmail.com>
2017-03-31 10:55:20 +0530

    Modified logic for time zone

--- CodeGenUtility/src/main/java/com/ge/DataCodeGenerationUtility.java
+++ CodeGenUtility/src/main/java/com/ge/DataCodeGenerationUtility.java
@@ -1,11 +1,20 @@
 package com.ge;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DataCodeGenerationUtility {
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("CST"));
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DataCodeGenerationUtility.class, args);
 	}
